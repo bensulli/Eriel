cda790395f4044fdc6cb872f35e85b2ea64a5122
bensulli <git@sulli.ca>
2013-12-10 13:00:59 -0800

    Interim changes, working layout nearly complete

--- gen/ca/sulli/eriel/R.java
+++ gen/ca/sulli/eriel/R.java
@@ -27,12 +27,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int healthLabelTxt=0x7f090001;
-        public static final int healthTxt=0x7f090003;
-        public static final int manaLabelTxt=0x7f090002;
-        public static final int manaTxt=0x7f090004;
-        public static final int pageNumTxt=0x7f090000;
+        public static final int action_settings=0x7f09000c;
+        public static final int choice1Btn=0x7f090006;
+        public static final int choice2Btn=0x7f090007;
+        public static final int choice3Btn=0x7f090008;
+        public static final int contentRelativeLayout=0x7f090004;
+        public static final int contentTxt=0x7f090005;
+        public static final int dividerLayout=0x7f090009;
+        public static final int healthBar=0x7f09000b;
+        public static final int healthLabelTxt=0x7f09000a;
+        public static final int masterRelativeLayout=0x7f090000;
+        public static final int pageNumTxt=0x7f090001;
+        public static final int playerBarLay=0x7f090003;
+        public static final int scrollView1=0x7f090002;
     }
     public static final class layout {
         public static final int activity_eriel_launcher=0x7f030000;
