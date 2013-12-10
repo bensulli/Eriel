d794e785de527de718876c542a1966bb5f3e9c67
bensulli <git@sulli.ca>
2013-12-10 10:30:32 -0800

    Really? What did I do?

--- gen/ca/sulli/eriel/R.java
+++ gen/ca/sulli/eriel/R.java
@@ -27,8 +27,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090001;
-        public static final int textView1=0x7f090000;
+        public static final int action_settings=0x7f090005;
+        public static final int healthLabelTxt=0x7f090001;
+        public static final int healthTxt=0x7f090003;
+        public static final int manaLabelTxt=0x7f090002;
+        public static final int manaTxt=0x7f090004;
+        public static final int pageNumTxt=0x7f090000;
     }
     public static final class layout {
         public static final int activity_eriel_launcher=0x7f030000;
