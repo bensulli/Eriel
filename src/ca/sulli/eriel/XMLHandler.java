e85c74505f95e41a8c0b9f5846f74b73511d793b
bensulli <git@sulli.ca>
2013-12-11 14:46:45 -0800

    XMLHandler class added, but useless

---
+++ src/ca/sulli/eriel/XMLHandler.java
@@ -0,0 +1,18 @@
+package ca.sulli.eriel;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import android.app.Activity;
+
+public class XMLHandler extends Activity {
+
+	
+	
+}
